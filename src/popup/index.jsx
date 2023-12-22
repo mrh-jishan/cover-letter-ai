03410541976149fa7cc1d7da727ab29985cde44d
robin-hassan <robiul.hassan12102@gmail.com>
2023-12-22 02:21:22 -0500

    init setup with content script

---
+++ src/popup/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Popup from './Popup.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+)
+
+
+
+// "matches": ["https://www.linkedin.com/*"],
+// "css": [
+//   "assets/css/content.css"
+// ],
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        content: 'src/content/index.jsx',
+        popup: 'src/popup/index.html',
+      },
+      output: {
+        entryFileNames: 'src/[name]/index.js',
+        chunkFileNames: 'assets/js/[name].js',
+        assetFileNames: 'assets/[ext]/[name].[ext]'
+      },
+    },
+  },
+})
